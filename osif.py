923854bc6d473888c42ce3ece7fa8dec2d8b3f72
sonydevv <141344102+sonydevv@users.noreply.github.com>
2023-09-10 20:10:54 +0700

    Update osif.py

--- osif.py
+++ osif.py
@@ -3,7 +3,7 @@
 import json , sys , hashlib , os , time , marshal, getpass
 ###################################################################
 '''
-     Jangan Direcode ya bosku , tinggal make apa susahnya sihh
+     Jangan Direcode ya bosku , tinggal make apa susahnya sih
 '''
 ###################################################################
 #                             COLOR
